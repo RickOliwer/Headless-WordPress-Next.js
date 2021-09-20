80eec95e1949b6594b9fb05327a06470a7bcc84b
Rick <rickoliwer@gmail.com>
2021-09-20 17:41:35 +0200

    Fetch menus from wordpress

--- wp_headless/pages/index.js
+++ wp_headless/pages/index.js
@@ -1,10 +1,35 @@
 import Head from 'next/head'
+import Client from '../src/apollo/client'
+import { GET_MENUS } from '../src/queries/get-menus'
 
-export default function Home() {
+
+export default function Home( {menus} ) {
+  console.log('my menus', menus);
   return (
     <div>
       hello world
     
     </div>
   )
+}
+
+
+
+
+export async function getStaticProps(){
+  const {data, loading, networkStatus} = await Client.query({
+    query: GET_MENUS
+  })
+
+  console.warn( 'data', data );
+
+  return {
+    props: {
+      menus: {
+        headerMenus: data?.headerMenus?.edges,
+        footerMenus: data?.footerMenus?.edges,
+      }
+      
+    },
+  }
 }
