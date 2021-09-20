bbb8c13d89556793cd77a036f26c638ac6d9be02
Rick <rickoliwer@gmail.com>
2021-09-20 16:40:45 +0200

    setting up apollo client

--- wp_headless/src/apollo/client.js
+++ wp_headless/src/apollo/client.js
@@ -1,6 +1,15 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
 
-const client = new ApolloClient({
-    uri: '',
-    cache: new InMemoryCache,
+const auth = Buffer.from(
+    process.env.WP_HEADLESS_GRAPHQL_AUTH_USER + ':' + process.env.WP_HEADLESS_GRAPHQL_AUTH_PASS
+).toString('base64')
+
+const Client = new ApolloClient({
+    uri: process.env.WP_HEADLESS_GRAPHQL_URI,
+    headers: {
+        Authorization: `Basic ${auth}`,
+    },
+    cache: new InMemoryCache()
 })
+
+export default Client;
