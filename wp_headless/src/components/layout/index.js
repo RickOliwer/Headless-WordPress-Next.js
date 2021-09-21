a828a93a1315cbd18027b9f3ac127a3fc50e2495
Rick <rickoliwer@gmail.com>
2021-09-21 10:15:01 +0200

    favicon icon done

--- wp_headless/src/components/layout/index.js
+++ wp_headless/src/components/layout/index.js
@@ -1,10 +1,14 @@
 import Header from "./header";
 import Footer from "./footer";
+import head from 'next/head';
 
 const Layout = ({children, data}) => {
     console.log('my data', data);
     return ( 
         <div>
+            <head>
+                <link rel="shortcut icon" href={ data?.header?.favicon} />
+            </head>
             <Header header={data?.header} headerMenus={data?.menus?.headerMenus} />
             {children}
             <Footer footer={data?.footer} footerMenus={data?.menus?.footerMenus}/>
